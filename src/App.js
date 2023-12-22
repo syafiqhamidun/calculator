12c29100420e35a59c261ed508a0cfced3a77bd3
Syafiq Hamidun <syafiqhamidun10@gmail.com>
2023-12-22 22:17:08 +0800

    trial

--- src/App.js
+++ src/App.js
@@ -1,58 +1,60 @@
-import { useState } from 'react';
-import './App.css';
-import { clear } from '@testing-library/user-event/dist/clear';
-// import { useState } from 'react';
+import React, { useState } from 'react';
 
-function App() {
+// A simple React Calculator App
+const CalculatorApp = () => {
+  // State to store the input and result
+  const [input, setInput] = useState('');
+  const [result, setResult] = useState('');
 
-  const [result , setResult] = useState('');
+  // Function to handle when a button is pressed
+  const handleButtonClick = (value) => {
+    // If 'C' is pressed, clear the input and result
+    if (value === 'C') {
+      setInput('');
+      setResult('');
+    } else if (value === '=') {
+      // When '=' is pressed, try to evaluate the input
+      try {
+        const evalResult = eval(input);
+        setResult(evalResult);
+      } catch (error) {
+        setResult('Error');
+      }
+    } else {
+      // For other buttons, append their value to the input string
+      setInput(input + value);
+    }
+  };
 
-  let handleClick = (e) => {
-    setResult(result.concat(e.target.value))
-  }
-
-  const clearAnswer = () => {
-    setResult("")
-  }
-
-  const totalAnswer = () => {
-    setResult(eval(result).toString())
-  }
-  
+  // Render the calculator buttons and display
   return (
-    <div className='flex justify-center mt-60'>
-          <div className='bg-teal-800 w-1/4 h-96'>
-
-            <div className='flex justify-center mt-3'>
-              <input type="text" className='w-1/2 h-10 rounded-lg' placeholder='hello' value={result} />
-            </div>
-
-            <div className='bg-teal-200 w-1/2 mt-4 mx-auto h-72'>
-              <div className='flex justify-evenly'>
-              <button className='bg-red-300 rounded w-12 h-12 mt-3' value='7' onClick={handleClick} >8</button>
-              <button className='bg-red-300 rounded w-12 h-12' value='8' onClick={handleClick} >8</button>
-              <button className='bg-red-300 rounded w-28 h-12' onClick={clearAnswer} >clear</button>
-              </div>
-
-              <div className='flex justify-evenly '>
-              <button className='bg-red-300 rounded w-12 h-12 mt-3' value='+' onClick={handleClick} >+</button>
-              <button className='bg-red-300 rounded w-12 h-12' value='' onClick={handleClick} >8</button>
-              <button className='bg-red-300 rounded w-12 h-12' value='' onClick={handleClick} >8</button>
-              <button className='bg-red-300 rounded w-12 h-12' value='' onClick={handleClick} >8</button>
-              </div>
-
-              <div className='flex justify-evenly '>
-              <button className='bg-red-300 rounded w-12 h-12 mt-3' onClick={totalAnswer} >=</button>
-              <button className='bg-red-300 rounded w-12 h-12' value='' onClick={handleClick} >8</button>
-              <button className='bg-red-300 rounded w-12 h-12' value='' onClick={handleClick} >8</button>
-              <button className='bg-red-300 rounded w-12 h-12' value='' onClick={handleClick} >8</button>
-              </div>
-
-            </div>
+    <div className="calculator">
+      <div className="display">
+        {input || "0"} {/* Show the input or '0' */}
+      </div>
+      <div className="result">
+        {result}
+      </div>
+      <div className="buttons">
+        <button onClick={() => handleButtonClick('1')}>1</button>
+        <button onClick={() => handleButtonClick('2')}>2</button>
+        <button onClick={() => handleButtonClick('3')}>3</button>
+        <button onClick={() => handleButtonClick('+')}>+</button>
+        <button onClick={() => handleButtonClick('4')}>4</button>
+        <button onClick={() => handleButtonClick('5')}>5</button>
+        <button onClick={() => handleButtonClick('6')}>6</button>
+        <button onClick={() => handleButtonClick('-')}>-</button>
+        <button onClick={() => handleButtonClick('7')}>7</button>
+        <button onClick={() => handleButtonClick('8')}>8</button>
+        <button onClick={() => handleButtonClick('9')}>9</button>
+        <button onClick={() => handleButtonClick('*')}>*</button>
+        <button onClick={() => handleButtonClick('0')}>0</button>
+        <button onClick={() => handleButtonClick('C')}>C</button>
+        <button onClick={() => handleButtonClick('=')}>=</button>
+        <button onClick={() => handleButtonClick('/')}>/</button>
+      </div>
     </div>
-    </div>
-
   );
-}
+};
 
-export default App;
+export default CalculatorApp;
